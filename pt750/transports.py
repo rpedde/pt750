0e212572042768183bf5a0944f115c768674afa7
rpedde <ron@pedde.com>
2022-04-11 08:07:17 -0500

    first pass

--- pt750/transports.py
+++ pt750/transports.py
@@ -8,10 +8,10 @@ class LabelPrinter:
     def __init__(self, uri):
         self.uri = uri
         parts = urlparse(self.uri)
-        if parts.scheme == 'tcp':
+        if parts.scheme == "tcp":
             self.transport = TCPTransport(self.uri)
         else:
-            raise RuntimeError('cannot find transport')
+            raise RuntimeError("cannot find transport")
 
 
 class PT750W(LabelPrinter):
@@ -22,13 +22,13 @@ class PT750W(LabelPrinter):
         image_data = bytearray(img.tobytes())
 
         for idx in range(len(image_data)):
-            image_data[idx] = image_data[idx] ^ 0xff
+            image_data[idx] = image_data[idx] ^ 0xFF
 
-        bytes = b'\x00' * 100  # reset stream
-        bytes += b'\x1B\x40'  # inittialize
-        bytes += b'\x1B\x69\x4D\x40'  # auto tape cut
-        bytes += b'\x1B\x69\x4B\x08'  # no chain printing, low res (128?)
-        bytes += b'\x4d\x02'  # compression
+        bytes = b"\x00" * 100  # reset stream
+        bytes += b"\x1B\x40"  # inittialize
+        bytes += b"\x1B\x69\x4D\x40"  # auto tape cut
+        bytes += b"\x1B\x69\x4B\x08"  # no chain printing, low res (128?)
+        bytes += b"\x4d\x02"  # compression
 
         label_width = img.width
         label_height = img.height
@@ -38,13 +38,13 @@ class PT750W(LabelPrinter):
         for line in range(label_height):
             bytes_per_line = label_width // 8
             byte_ofs = bytes_per_line * line
-            row_bytes = image_data[byte_ofs:byte_ofs + bytes_per_line]
+            row_bytes = image_data[byte_ofs : byte_ofs + bytes_per_line]  # noqa: E203
 
             # raster graphics transfer
-            bytes += b'\x47\x11\x00\x0F'  # "compressed" 16 literal bytes
+            bytes += b"\x47\x11\x00\x0F"  # "compressed" 16 literal bytes
             bytes += row_bytes
 
-        bytes += b'\x1a'  # print and feed
+        bytes += b"\x1a"  # print and feed
 
         self.transport.send_bytes(bytes)
 
